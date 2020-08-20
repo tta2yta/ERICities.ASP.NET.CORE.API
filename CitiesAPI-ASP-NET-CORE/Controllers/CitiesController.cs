7e3006b9d9b0f55e84e87f02aad3fe342e6cbf5f
tta2yta@gmail.com <tta2yta@gmail.com>
2020-08-20 18:22:55 +0300

    adding cities controller api

---
+++ CitiesAPI-ASP-NET-CORE/Controllers/CitiesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CitiesAPI.ASP.NET.CORE.Models;
+
+namespace CitiesAPI.ASP.NET.CORE.Controllers
+{
+  //[Route("api/[controller]")]
+    [Route("api/cities")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+
+      //  [HttpGet("api/cities")]
+     // [HttpGet]
+        /*  public JsonResult GetCities()
+          {
+              return new JsonResult(new List<object>(){
+                     new {id=1, name="Asmara"},
+                     new{id=2, name="Keren"}
+              }); 
+          }*/
+        [HttpGet]
+        public ActionResult GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Getcity(int id)
+        {
+            var returnCity = CitiesDataStore.Current.Cities.
+                FirstOrDefault(c => c.Id == id);
+            if (returnCity == null)
+            {
+                return NotFound();
+            }
+            return Ok(returnCity);
+        }
+    }
+}
