ff9bb7f57517150ba5a72b4c64ad4a900174c380
tta2yta@gmail.com <tta2yta@gmail.com>
2020-08-24 01:27:57 +0300

    poitofinterest for city fully upadate api

--- CitiesAPI-ASP-NET-CORE/Controllers/PointsOfInterestController.cs
+++ CitiesAPI-ASP-NET-CORE/Controllers/PointsOfInterestController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CitiesAPI.ASP.NET.CORE.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
@@ -80,6 +81,74 @@ namespace CitiesAPI.ASP.NET.CORE.Controllers
 
             return CreatedAtRoute("GetPointOfInterest",
                 new { city, id = newPointOfInterest.Id }, newPointOfInterest);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdatePointOfInterest(int cityid, int id, 
+            [FromBody] PointOfInterestUpdateDto pointOfInterestUpdateDto)
+        {
+            if (pointOfInterestUpdateDto.name == pointOfInterestUpdateDto.description)
+            {
+                ModelState.AddModelError(
+                    "description",
+                    "Please provide another value, name and description should not be thesame");
+            }
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityid);
+            if(city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestFromStore = city.PointsOfInterest.FirstOrDefault(
+                p => p.Id == id);
+            if (pointOfInterestFromStore == null)
+                return NotFound();
+            pointOfInterestFromStore.Name = pointOfInterestUpdateDto.name;
+            pointOfInterestFromStore.Name = pointOfInterestUpdateDto.name;
+
+            return NoContent();
+
+        }
+
+        public IActionResult partiallyUpadatePointOfInterest(int cityid, int id,
+            [FromBody] JsonPatchDocument<PointOfInterestUpdateDto> patchDoc)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityid);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestFromStore = city.PointsOfInterest.FirstOrDefault(
+                p => p.Id == id);
+            if (pointOfInterestFromStore == null)
+                return NotFound();
+
+            var pointOfInterestPatch = new PointOfInterestUpdateDto()
+            {
+                name = pointOfInterestFromStore.Name,
+                description = pointOfInterestFromStore.Description
+            };
+            patchDoc.ApplyTo(pointOfInterestPatch, ModelState);
+
+            if (pointOfInterestPatch.name == pointOfInterestPatch.description)
+            {
+                ModelState.AddModelError(
+                    "description",
+                    "Please provide another value, name and description should not be thesame");
+            }
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            if (!TryValidateModel(pointOfInterestPatch))
+                return BadRequest();
+
+            pointOfInterestFromStore.Name = pointOfInterestPatch.name;
+            pointOfInterestFromStore.Name = pointOfInterestPatch.name;
+
+            return NoContent();
         }
     }
 }
---
+++ CitiesAPI-ASP-NET-CORE/Models/PointOfInterestUpdateDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CitiesAPI.ASP.NET.CORE.Models
+{
+    public class PointOfInterestUpdateDto
+    {
+        [Required(ErrorMessage = "You should provide name value")]
+        [MaxLength(50)]
+        public string name { get; set; }
+
+        [MaxLength(200)]
+        public string description { get; set; }
+    }
+}
