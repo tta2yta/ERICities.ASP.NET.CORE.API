4c6fc165e89913e82a1702e4fcf636914b3f194a
tta2yta@gmail.com <tta2yta@gmail.com>
2020-08-24 23:56:57 +0300

    adding logger

--- CitiesAPI-ASP-NET-CORE/Controllers/PointsOfInterestController.cs
+++ CitiesAPI-ASP-NET-CORE/Controllers/PointsOfInterestController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Logging;
 
 namespace CitiesAPI.ASP.NET.CORE.Controllers
 {
@@ -15,16 +16,37 @@ namespace CitiesAPI.ASP.NET.CORE.Controllers
     [ApiController]
     public class PointsOfInterestController : ControllerBase
     {
+
+        private readonly ILogger<PointsOfInterestController> _logger;
+
+        public PointsOfInterestController (ILogger<PointsOfInterestController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+
         [HttpGet]
         public IActionResult GetPointsOfInterest(int cityid)
         {
-            var city = CitiesDataStore.Current.Cities
-                .FirstOrDefault(c => c.Id == cityid);
+            try
+            {
+              //  throw new Exception("Sample Exception");
+                var city = CitiesDataStore.Current.Cities
+                    .FirstOrDefault(c => c.Id == cityid);
 
-            if (city == null)
-                return NotFound();
+                if (city == null)
+                {
+                    _logger.LogInformation($"City with citi id {cityid} is not found ");
+                    return NotFound();
+                }
 
-            return Ok(city.PointsOfInterest);
+                return Ok(city.PointsOfInterest);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogInformation($"Exception raised while getting point of interest from city");
+                return StatusCode(500, "A proplem arises while handling you request");
+            }
 
 
         }
