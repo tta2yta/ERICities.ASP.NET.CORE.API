426ca32c6aee15062376cd20451694a36eec0392
tta2yta@gmail.com <tta2yta@gmail.com>
2020-08-25 16:01:06 +0300

    adding configuartionfile

--- CitiesAPI-ASP-NET-CORE/Services/CloudMailServices.cs
+++ CitiesAPI-ASP-NET-CORE/Services/CloudMailServices.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -8,11 +9,22 @@ namespace CitiesAPI.ASP.NET.CORE.Services
 {
     public class CloudMailServices
     {
-        private string _mailTo = "tta2yta@gmail.com";
-        private string _mailFrom = "m1909652@edu.misis.ru";
+        private readonly IConfiguration _configuration;
+        //private string _mailTo = "tta2yta@gmail.com";
+        //private string _mailFrom = "m1909652@edu.misis.ru";
+
+        public CloudMailServices(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
         public void send(string subject, string message)
         {
-            Debug.WriteLine($"Mail from {_mailFrom} to {_mailTo}  with Cloud Service");
+            /* Debug.WriteLine($"Mail from {_mailFrom} to {_mailTo}  with Cloud Service");
+             Debug.WriteLine($"Subject :{subject}");
+             Debug.WriteLine($"Message: {message}");*/
+
+            Debug.WriteLine($"Mail from {_configuration["mailSettings:mailToAdsress"]} to {_configuration["mailSettings:mailFromAdress"]}  with Local Service");
             Debug.WriteLine($"Subject :{subject}");
             Debug.WriteLine($"Message: {message}");
         }
