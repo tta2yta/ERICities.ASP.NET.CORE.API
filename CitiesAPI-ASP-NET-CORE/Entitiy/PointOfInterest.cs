ba006747ec5d3dfa77e4d399f1af415771a46f1d
unknown <tta2yta@gmail.com>
2020-08-30 22:12:17 +0300

    adding entities

---
+++ CitiesAPI-ASP-NET-CORE/Entitiy/PointOfInterest.cs
@@ -0,0 +1,11 @@
+﻿namespace CitiesAPI.ASP.NET.CORE.Entitiy
+{
+    public class PointOfInterest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public City  City { set; get; }
+        public int CityId { get; set; }
+    }
+}
