e49eb52dce32773d835daceedcbdfe20a80afc36
unknown <tta2yta@gmail.com>
2020-08-31 13:05:30 +0300

    adding dbcontext

--- CitiesAPI-ASP-NET-CORE/Startup.cs
+++ CitiesAPI-ASP-NET-CORE/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CitiesAPI.ASP.NET.CORE.Context;
 using CitiesAPI.ASP.NET.CORE.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -37,7 +39,13 @@ namespace CitiesAPI_ASP_NET_CORE
 #else
             services.AddTransient<IMailService, CloudMailServices>();
 #endif
+
+            services.AddDbContext<CityInfoContext>(o=> 
+            o.UseSqlServer(Configuration.GetConnectionString("connectionstring"));
+
         }
+
+        
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
