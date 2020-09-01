ff29a98717e9e434bce696a6ea31e3e037d9aee7
unknown <tta2yta@gmail.com>
2020-09-02 00:22:10 +0300

    implement pointofinterest

--- CitiesAPI-ASP-NET-CORE/Services/CityInfoRepository.cs
+++ CitiesAPI-ASP-NET-CORE/Services/CityInfoRepository.cs
@@ -1,5 +1,6 @@
 ﻿using CitiesAPI.ASP.NET.CORE.Context;
 using CitiesAPI.ASP.NET.CORE.Entitiy;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,14 +21,20 @@ namespace CitiesAPI.ASP.NET.CORE.Services
             return _context.Cities.OrderBy(c=> c.Name).ToList();
         }
 
-        public City GetCity(int cityId)
+        public City GetCity(int cityId, bool includePointOfInterest)
         {
-            throw new NotImplementedException();
+            if (includePointOfInterest)
+            {
+                return _context.Cities.Include(c => c.pointOfInterests)
+                    .Where(p => p.Id == cityId).FirstOrDefault();
+            }
+            return _context.Cities.Where(c => c.Id == cityId).FirstOrDefault();
         }
 
         public PointOfInterest GetPointOfInterest(int cityId, int pointOfInterstId)
         {
-            throw new NotImplementedException();
+            return _context.PointOfInterests.Where(c => c.CityId == cityId &&
+            c.Id == pointOfInterstId).FirstOrDefault();
         }
 
         public IEnumerable<PointOfInterest> GetPointOfInterests(int cityId)
