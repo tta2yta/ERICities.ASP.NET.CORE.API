aacde3b66951cdc360b113c8f4ac71abd1e30a3e
unknown <tta2yta@gmail.com>
2020-09-02 00:16:20 +0300

    implement getcity

--- CitiesAPI-ASP-NET-CORE/Services/ICityInfoRepository.cs
+++ CitiesAPI-ASP-NET-CORE/Services/ICityInfoRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CitiesAPI.ASP.NET.CORE.Entitiy;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,9 @@ namespace CitiesAPI.ASP.NET.CORE.Services
 {
     interface ICityInfoRepository
     {
+        IEnumerable<City> GetCities();
+        City GetCity(int cityId, bool includePointOfInterest);
+        IEnumerable<PointOfInterest> GetPointOfInterests(int cityId);
+        PointOfInterest GetPointOfInterest(int cityId, int pointOfInterstId );
     }
 }
