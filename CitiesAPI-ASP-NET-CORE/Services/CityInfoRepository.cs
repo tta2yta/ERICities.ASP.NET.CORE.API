9d4433307096cd05a8eb1ca23cbca641ee0fe34c
unknown <tta2yta@gmail.com>
2020-09-02 00:00:44 +0300

    adding CityInfoclass repository

---
+++ CitiesAPI-ASP-NET-CORE/Services/CityInfoRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CitiesAPI.ASP.NET.CORE.Services
+{
+    public class CityInfoRepository
+    {
+    }
+}
