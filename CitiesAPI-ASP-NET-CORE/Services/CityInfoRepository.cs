ea593535f7f6bcbf1020a151b61d36cbddbcb2f3
unknown <tta2yta@gmail.com>
2020-09-02 00:10:25 +0300

    implemet getcities

--- CitiesAPI-ASP-NET-CORE/Services/CityInfoRepository.cs
+++ CitiesAPI-ASP-NET-CORE/Services/CityInfoRepository.cs
@@ -1,11 +1,38 @@
-﻿using System;
+﻿using CitiesAPI.ASP.NET.CORE.Context;
+using CitiesAPI.ASP.NET.CORE.Entitiy;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CitiesAPI.ASP.NET.CORE.Services
 {
-    public class CityInfoRepository
+    public class CityInfoRepository : ICityInfoRepository
     {
+        private readonly CityInfoContext _context;
+
+        public CityInfoRepository( CityInfoContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<City> GetCities()
+        {
+            return _context.Cities.OrderBy(c=> c.Name).ToList();
+        }
+
+        public City GetCity(int cityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PointOfInterest GetPointOfInterest(int cityId, int pointOfInterstId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PointOfInterest> GetPointOfInterests(int cityId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
