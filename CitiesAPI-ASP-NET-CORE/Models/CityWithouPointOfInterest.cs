63c4f59035a2aee71b6ad7215e0e20934152844c
unknown <tta2yta@gmail.com>
2020-09-02 00:35:45 +0300

    adding citywithoutpointofinterest

---
+++ CitiesAPI-ASP-NET-CORE/Models/CityWithouPointOfInterest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CitiesAPI.ASP.NET.CORE.Models
+{
+    public class CityWithouPointOfInterest
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+    }
+}
