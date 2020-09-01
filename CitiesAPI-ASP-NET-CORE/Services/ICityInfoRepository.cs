c6608ded82272b46af530297e25b8a15d625fe8f
unknown <tta2yta@gmail.com>
2020-09-01 23:58:45 +0300

    adding ICityinforepository

---
+++ CitiesAPI-ASP-NET-CORE/Services/ICityInfoRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CitiesAPI.ASP.NET.CORE.Services
+{
+    interface ICityInfoRepository
+    {
+    }
+}
